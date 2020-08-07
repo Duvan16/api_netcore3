44af07b643c46468ff25e6da3ef6ad67a58ef98a
Duvan <duvan4448@gmail.com>
2020-08-07 18:03:56 -0500

    1-ConfigurarEntityFrameworkCore

---
+++ api_netcore3/api_netcore3/Contexts/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using api_netcore3.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api_netcore3.Contexts
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            :base(options)
+        {
+
+        }
+
+        public DbSet<Autor> Autores { get; set; }
+    }
+}
