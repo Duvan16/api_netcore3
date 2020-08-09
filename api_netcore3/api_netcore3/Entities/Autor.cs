ce82a8bef1d09be62ad6253b6f33f3c8d97dd11a
Duvan <duvan4448@gmail.com>
2020-08-08 22:45:30 -0500

    7-ValidacionesPersonalizadas

--- api_netcore3/api_netcore3/Entities/Autor.cs
+++ api_netcore3/api_netcore3/Entities/Autor.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using api_netcore3.Helpers;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -6,11 +7,12 @@ using System.Threading.Tasks;
 
 namespace api_netcore3.Entities
 {
-    public class Autor
+    public class Autor: IValidatableObject
     {
         public int Id { get; set; }
         
         [Required]
+        //[PrimeraLetraMayuscula]
         [StringLength(10,ErrorMessage ="El campo Nombre debe tener {1} caracteres o menos")]
         public string Nombre { get; set; }
 
@@ -24,5 +26,18 @@ namespace api_netcore3.Entities
         public string Url { get; set; }
 
         public List<Libro> Libros { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (!string.IsNullOrEmpty(Nombre))
+            {
+                var firstLetter = Nombre[0].ToString();
+
+                if (firstLetter != firstLetter.ToUpper())
+                {
+                    yield return new ValidationResult("La primera letra debe ser mayúscula", new string[] { nameof(Nombre)});
+                }
+            }
+        }
     }
 }
